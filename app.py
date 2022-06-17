1abfc31f88f418e51ed8826d7f2ae27b0a5778db
jeh362 <99925709+jeh362@users.noreply.github.com>
2022-06-17 15:08:36 -0400

    added requirements.txt and db.py file with basic structure, also added to app.py file with basic structure as well

---
+++ app.py
@@ -0,0 +1,225 @@
+import json 
+
+from db import db
+from db import Victory
+from db import User 
+from db import Asset
+
+import users_dao
+
+import datetime
+import random
+
+from flask import Flask
+from flask import request 
+
+import requests
+
+import os
+
+# Third-party libraries
+from flask import Flask, redirect, request, url_for
+
+# google libraries
+from google.oauth2 import id_token
+from google.auth.transport import requests
+import requests
+
+# define db filename 
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
+db_filename = "bukethaca.db"
+
+# setup config 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
+# initialize app
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+# generalized response formats 
+def success_response(data, code=200):
+    """
+    Generalized success response function
+    """
+    return json.dumps(data), code
+
+def failure_response(message, code=404):
+    """
+    Generalized failure response function
+    """
+    return json.dumps({"error": message}), code
+
+# -- GOOGLE ROUTES ------------------------------------------------------
+@app.route("/api/login/", methods=["POST"])
+def login():
+    """
+    Endpoint for logging a user in with Google and registering new users
+    """
+    data = json.loads(request.data)
+    token = data.get("token")
+    try:
+        
+        id_info = id_token.verify_oauth2_token(token, requests.Request(), os.environ.get("CLIENT_ID"))
+        email, first_name, last_name = id_info["email"], id_info["given_name"], id_info["family_name"]
+        name = first_name + " " + last_name
+        
+        user = User.query.filter_by(email=email).first()
+
+        if user is None:
+            # create user and session
+            user = User(email=email, name=name)
+            db.session.add(user)
+            db.session.commit()
+        else:
+            user.renew_session()
+            db.session.commit()
+
+        return success_response(user.serialize())
+        # return session serialize
+    except ValueError:
+        raise Exception("Invalid Token")
+
+@app.route("/logout/", methods=["POST"])
+def logout():
+    """
+    ?? Endpoint for logging a user out
+    """
+    was_successful, session_token = extract_token(request)
+    if not was_successful:
+        return session_token
+    
+    user = users_dao.get_user_by_session_token(session_token)
+    if not user or not user.verify_session_token(session_token):
+        return failure_response("Invalid session token")
+    user.session_expiration = datetime.datetime.now()
+    db.session.commit()
+
+@app.route("/api/users/<int:user_id>/phone/", methods=["POST"])
+def add_number(user_id):
+    """
+    Endpoint for adding phone number to user
+    """
+    body = json.loads(request.data)
+    number = body.get("number")
+    if number is None:
+        return failure_response("Please input a phone number", 400)
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found", 404)
+    user.number = number
+    db.session.commit()
+    return success_response(user.serialize())
+
+
+# -- USER ROUTES ------------------------------------------------------
+
+@app.route("/api/users/<int:user_id>/")
+def get_specific_user(user_id):
+    """
+    Endpoint for getting user by id 
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+    return success_response(user.serialize())
+
+@app.route("/api/user/<int:user_id>/")
+def delete_user(user_id):
+    """
+    Endpoint for deleting a user 
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+    db.session.delete(user)
+    db.session.commit()
+    return success_response(user.serialize())
+
+
+# -- VICTORY ROUTES ------------------------------------------------------
+@app.route("/api/victories/")
+def get_all_victories():
+    """
+    Endpoint for getting all victories
+    """
+    # return success_response({"victories": [e.serialize() for e in Event.query.order_by(Event.date.desc())]})
+    return success_response({"victories": [e.serialize() for e in Victory.query.all()]})
+    
+@app.route("/api/users/<int:user_id>/victories/", methods=["POST"])
+def create_victory(user_id):
+    """
+    Endpoint for creating a victory
+
+    """
+    # checks if user exists
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+
+    body = json.loads(request.data)
+    date = body.get("date")
+    if date is None:
+        return failure_response("Please put something for date", 400)  
+    description = body.get("description")
+    if description is None:
+        return failure_response("Please put something for the description", 400) 
+    image_data = body.get("image_data")
+    if image_data is None:
+            return failure_response("No base64 image passed in!", 400)
+    
+    # creates image object 
+    image = Asset(image_data=image_data)
+    db.session.add(image)
+    db.session.commit()
+    
+    # creates event object 
+    new_victory = Victory(date=date,description=description, image_id=image.id)
+    db.session.add(new_victory)
+    # adds event to user created
+    user.created_events.append(new_victory)
+    db.session.commit()
+    return success_response(new_victory.serialize(), 201)
+
+@app.route("/api/victories/<int:victory_id>/")
+def get_specific_victory(victory_id):
+    """
+    Endpoint for getting a event by id 
+    """
+    # checks if event exists
+    victory= Victory.query.filter_by(id=victory_id).first()
+    if victory is None:
+        return failure_response("Sorry, victory was not found.")
+    return success_response(victory.serialize())
+           
+
+@app.route("/api/users/<int:user_id>/events/<int:event_id>/", methods=["DELETE"])
+def delete_victory(user_id,victory_id):
+    """
+    Endpoint for deleting an event by id
+    """
+    # checks if user exists
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+    # checks if victory exists
+    victory = Victory.query.filter_by(id=victory_id).first()
+    if victory is None:
+        return failure_response("Event not found!")
+    # checks if user created the victory
+    if victory not in user.created_victories:
+        return failure_response("User did not create this event!")
+    db.session.delete(victory)
+    db.session.commit()
+    return success_response(victory.serialize())
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ db.py
@@ -0,0 +1,223 @@
+import code
+from flask_sqlalchemy import SQLAlchemy
+
+import base64
+import boto3
+import datetime
+import io
+from io import BytesIO
+from mimetypes import guess_extension, guess_type
+import os
+from PIL import Image
+import random
+import re
+import string
+
+import hashlib
+
+from sqlalchemy import ForeignKey
+import bcrypt
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """
+    User model 
+
+    one-to-many relationships with Events table
+    """
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    number = db.Column(db.Integer, nullable=True)
+
+    # Session information
+    session_token = db.Column(db.String, nullable=False, unique=True)
+    session_expiration = db.Column(db.DateTime, nullable=False)
+    update_token = db.Column(db.String, nullable=False, unique=True)
+
+    def _init_(self, **kwargs):
+        """
+        Initialize User object/entry
+        """
+        self.name = kwargs.get("name")
+        self.email = kwargs.get("email")
+        self.renew_session()
+
+    def _urlsafe_base_64(self):
+        """
+        Randomly generates hashed tokens (used for session/update tokens)
+        """
+        return hashlib.sha1(os.urandom(64)).hexdigest()
+
+    def renew_session(self):
+        """
+        Renews the sessions, i.e.
+        1. Creates a new session token
+        2. Sets the expiration time of the session to be a day from now
+        3. Creates a new update token
+        """
+        self.session_token = self._urlsafe_base_64()
+        self.session_expiration = datetime.datetime.now() + datetime.timedelta(days=1)
+        self.update_token = self._urlsafe_base_64()
+
+    def verify_update_token(self, update_token):
+        """
+        Verifies the update token of a user
+        """
+        return update_token == self.update_token
+
+
+    def serialize(self):
+        """
+        Serializes User object
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "number": self.number
+        }
+
+
+class Victory(db.Model):
+    """
+    Victory model 
+
+    many-to-one relationship with user
+    """
+
+    __tablename__ = "events"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    image_id = db.Column(db.Integer, db.ForeignKey("assets.id"), nullable=False)
+
+
+    def _init_(self, **kwargs):
+        """
+        Initialize Victory object
+        """
+        self.date = kwargs.get("date")
+        self.description = kwargs.get("description")
+        self.image_id = kwargs.get("image_id")
+        
+    def serialize(self):
+        """
+        Serializes Victory object
+        """
+        asset = Asset.query.filter_by(id=self.image_id).first()
+        return {
+            "id": self.id,
+            "date": self.date,
+            "description": self.description,
+            "image": asset.serialize()
+        }
+
+
+EXTENSIONS = ["png", "gif", "jpg", "jpeg"]
+BASE_DIR = os.getcwd()
+S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
+S3_BASE_URL = f"https://{S3_BUCKET_NAME}.s3.us-east-2.amazonaws.com" 
+
+class Asset(db.Model):
+    """
+    Asset Model
+
+    Has a one-to-one relationship with Event table
+    """
+    __tablename__ = "assets"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    base_url = db.Column(db.String, nullable=True)
+    salt =  db.Column(db.String, nullable=False)
+    extension =  db.Column(db.String, nullable=False)
+    width = db.Column(db.Integer, nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+
+    def __init__(self,**kwargs):
+        """
+        Initializes an Asset object/entry
+        """
+        self.event_id = kwargs.get("event_id")
+        self.create(kwargs.get("image_data"))
+
+    def serialize(self):
+        """
+        Serialize Asset object
+        """
+        return f"{self.base_url}/{self.salt}.{self.extension}"
+
+    def event_serialize(self):
+        """
+        Serialize Asset object
+        """
+        return f"{self.base_url}/{self.salt}.{self.extension}"
+
+    def create(self, image_data):
+        """
+        Given an image in base64 form, it
+        1. Rejects the image is the filetype is not supported file type
+        2. Generates a random string for the image file name
+        3. Decodes the image and attempts to upload it to AWS
+        """
+        try:
+            ext = guess_extension(guess_type(image_data)[0])[1:]
+
+            #only accepts supported file types
+            if ext not in EXTENSIONS:
+                raise Exception(f"Unsupported file type: {ext}")
+
+
+            #generate random strong name for file
+            salt = "".join(
+                random.SystemRandom().choice(
+                    string.ascii_uppercase+ string.digits
+                )
+                for _ in range(16)
+            )
+
+            #decode the image and upload to aws
+            #remove header of base64 string
+            img_str = re.sub("^data:image/.+;base64,", "", image_data)
+            img_data = base64.b64decode(img_str)
+            img = Image.open(BytesIO(img_data))
+
+            self.base_url = S3_BASE_URL
+            self.salt = salt
+            self.extension = ext
+            self.width = img.width
+            self.height = img.height
+
+            img_filename = f"{self.salt}.{self.extension}"
+            self.upload(img, img_filename)
+
+        except Exception as e:
+            print(f"Error when creating image: {e}")
+
+    def upload(self, img, img_filename):
+        """
+        Attempt to upload the image to the specified S3 bucket
+        """
+        try:
+            # save image temporarily on server
+            img_temploc = f"{BASE_DIR}/{img_filename}"
+            img.save(img_temploc)
+            
+            # upload image to S3
+            s3_client = boto3.client("s3")
+            s3_client.upload_file(img_temploc, S3_BUCKET_NAME, img_filename)
+
+            # make s3 image url is public
+            s3_resource = boto3.resource("s3")
+            object_acl = s3_resource.ObjectAcl(S3_BUCKET_NAME, img_filename)
+            object_acl.put(ACL="public-read")
+
+            # removes image from server
+            os.remove(img_temploc)
+
+
+        except Exception as e:
+            print(f"Error when uploading image: {e}")
+
+
